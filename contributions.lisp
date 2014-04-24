89fca4978371eed716df777b8421fc5c8f079ff0
joe-w-bimedina <joeish80829@yahoo.com>
2014-04-23 22:34:58 -0700

    updated

--- contributions.lisp
+++ contributions.lisp
@@ -4,15 +4,16 @@
 ;;;;If you would like to contribute, see the Readme.txt for more details.
 
 
-;;Thank you to my tutor David Holz: http://www.grindwork.com/site/ who has helped mold me into the competent 
+;;Thank you to my tutor David Holz: http://www.grindwork.com who has helped mold me into the competent 
 ;;programmer I am today. He has contributed alot of info to my project thus far and given me the courage to 
 ;;excel at it. When approaching something new, I think "What would David Holz do" and that consistently 
 ;;makes me a winner
 
+;;Thanks to Arjun Comar for all his help and guidance on my project so far. https://github.com/arjuncomar
+;;He's an amazing programmer and a great guy to work with. Amnther guy I steal my ideas from.
+
 ;;Thank you to Jay Bromley for writing CL-OPENCV https://github.com/ryepup/cl-opencv. The knowledge 
 ;;gained from that library helped get started writing this project.
-
-;;Thanks to Arjun Comar for all his help and guidance on my project so far. https://github.com/arjuncomar
 
 ;;Thank you to OpenCV, the best Computer Vision library in the world. I mold their documentation into 
 ;;my own, because why mess with perfection.
